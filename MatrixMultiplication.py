d94b4e0ed24ded39ae842c294c4c8eeee7486a58
kor80 <cosimo.russo8080@gmail.com>
2022-06-24 20:02:27 +0200

    Recursive squared matrix multiplication, but it works only for exact power of two for now. The recurrence is:  T(n) = 8 T(n/2) + Theta(1)

--- MatrixMultiplication.py
+++ MatrixMultiplication.py
@@ -13,26 +13,24 @@ def matrix_multiply_r(A: np.ndarray, B: np.ndarray, C: np.ndarray, size: int):
 
 
 def matrix_multiply_recursive(A: np.ndarray, B: np.ndarray, C: np.ndarray, size: int, a: tuple, b: tuple, c: tuple):
-    print(a)
-    print(b)
-    print(c)
-    if size == 0:
-        C[c[0]][c[1]] += A[a[0]][a[1]] * B[b[0]][b[1]]
+    if size == 1:
+        C[c[0] - 1][c[1] - 1] += A[a[0] - 1][a[1] - 1] * B[b[0] - 1][b[1] - 1]
     else:
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0] // 2, a[1] // 2), (b[0] // 2, b[1] // 2), (c[0] // 2, c[1] // 2))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0] // 2, a[1]), (b[0], b[1] // 2), (c[0] // 2, c[1] // 2))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0] // 2, a[1] // 2), (b[0] // 2, b[1]), (c[0] // 2, c[1]))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0] // 2, a[1]), (b[0], b[1]), (c[0] // 2, c[1]))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0], a[1] // 2), (b[0] // 2, b[1] // 2), (c[0], c[1] // 2))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0], a[1]), (b[0], b[1] // 2), (c[0], c[1] // 2))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0], a[1] // 2), (b[0] // 2, b[1]), (c[0], c[1]))
-        matrix_multiply_recursive(A, B, C, size // 2, (a[0], a[1]), (b[0], b[1]), (c[0], c[1]))
+        half_size = size // 2
+        matrix_multiply_recursive(A, B, C, half_size, (a[0] - half_size, a[1] - half_size), (b[0] - half_size, b[1] - half_size), (c[0] - half_size, c[1] - half_size))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0] - half_size, a[1]), (b[0], b[1] - half_size), (c[0] - half_size, c[1] - half_size))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0] - half_size, a[1] - half_size), (b[0] - half_size, b[1]), (c[0] - half_size, c[1]))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0] - half_size, a[1]), (b[0], b[1]), (c[0] - half_size, c[1]))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0], a[1] - half_size), (b[0] - half_size, b[1] - half_size), (c[0], c[1] - half_size))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0], a[1]), (b[0], b[1] - half_size), (c[0], c[1] - half_size))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0], a[1] - half_size), (b[0] - half_size, b[1]), (c[0], c[1]))
+        matrix_multiply_recursive(A, B, C, half_size, (a[0], a[1]), (b[0], b[1]), (c[0], c[1]))
 
 
 if __name__ == '__main__':
-    n = 3
-    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    B = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    n = 4
+    A = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+    B = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
     C = np.zeros((n, n), dtype=int)
-    matrix_multiply_r(A, B, C, n-1)
+    matrix_multiply_r(A, B, C, n)
     print(C)
