1a09c371b04b4b9ba950a223c8529f2bda6b479d
kor80 <cosimo.russo8080@gmail.com>
2022-06-25 19:18:20 +0200

    Fixed: unable to print right child if left child is none.

--- DrawTree.py
+++ DrawTree.py
@@ -1,5 +1,5 @@
 import turtle
-# import BinaryTree as tree
+import BinaryTree as tree
 
 sc = turtle.Screen()
 t = turtle.Turtle()
@@ -7,9 +7,9 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 NODE_RADIUS = 15
 INFO_SIZE = 10
-EDGE_LENGTH = 200
+EDGE_LENGTH = 300
 STARTING_EDGE_ANGLE = 80
-EDGE_ANGLE_DECREASING_FACTOR = 20
+EDGE_ANGLE_DECREASING_FACTOR = 35
 
 
 def draw_node(info: int):
@@ -39,16 +39,14 @@ def draw_tree(root, angle):
     if root is None:
         return
     draw_node(root.info)
-    if root.lc is None:
-        return
-    position = t.pos()
-    draw_edge(-1, angle)
-    draw_tree(root.lc, angle - EDGE_ANGLE_DECREASING_FACTOR)
-    t.setpos(position)
-    if root.rc is None:
-        return
-    draw_edge(1, angle)
-    draw_tree(root.rc, angle - EDGE_ANGLE_DECREASING_FACTOR)
+    if root.lc is not None:
+        position = t.pos()
+        draw_edge(-1, angle)
+        draw_tree(root.lc, angle - EDGE_ANGLE_DECREASING_FACTOR)
+        t.setpos(position)
+    if root.rc is not None:
+        draw_edge(1, angle)
+        draw_tree(root.rc, angle - EDGE_ANGLE_DECREASING_FACTOR)
 
 
 if __name__ == '__main__':
@@ -63,14 +61,22 @@ if __name__ == '__main__':
     t.sety(SCREEN_HEIGHT / 3)
 
     # Test Values
-    # n1 = tree.BinaryTree(3, None, None)
-    # n2 = tree.BinaryTree(7, None, None)
-    # n3 = tree.BinaryTree(13, None, None)
-    # n4 = tree.BinaryTree(17, None, None)
-    # n5 = tree.BinaryTree(6, n1, n2)
-    # n6 = tree.BinaryTree(15, n3, n4)
-    # root = tree.BinaryTree(10, n5, n6)
+    n1 = tree.BinaryTree(3, None, None)
+    n2 = tree.BinaryTree(7, None, None)
+    n3 = tree.BinaryTree(13, None, None)
+    n4 = tree.BinaryTree(17, None, None)
+    n5 = tree.BinaryTree(3, None, None)
+    n6 = tree.BinaryTree(7, None, None)
+    n7 = tree.BinaryTree(13, None, None)
+    n8 = tree.BinaryTree(17, None, None)
+    n9 = tree.BinaryTree(6, n1, n2)
+    n10 = tree.BinaryTree(15, None, n4)
+    n11 = tree.BinaryTree(6, n5, n6)
+    n12 = tree.BinaryTree(15, n7, None)
+    n13 = tree.BinaryTree(6, n9, n10)
+    n14 = tree.BinaryTree(15, n11, n12)
+    root = tree.BinaryTree(10, n13, n14)
 
-    # draw_tree(root, STARTING_EDGE_ANGLE)
+    draw_tree(root, STARTING_EDGE_ANGLE)
 
     turtle.exitonclick()
